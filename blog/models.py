b74f89d5e8349230f16276718c12a9f421a8134d
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-03-24 18:21:41 +0300

    shrink some code.

--- blog/models.py
+++ blog/models.py
@@ -14,9 +14,7 @@ class PostQuerySet(models.QuerySet):
     def fetch_with_comments_count(self):
         """
         Add attribute 'comments_count'
-        :param field: field for count and  sort for  result
-        :param chart_length: the length of slice
-        :return: sorted list of posts with length pointed in argument
+        :return: list of posts with attribute 'comments_count'
         """
         popular_posts_ids = [post.id for post in self]
         posts_with_comments = Post.objects.filter(id__in=popular_posts_ids).annotate(Count('comments'))
@@ -35,7 +33,6 @@ class TagQuerySet(models.QuerySet):
 
 
 class Post(models.Model):
-    objects = PostQuerySet.as_manager()
     title = models.CharField('Заголовок', max_length=200)
     text = models.TextField('Текст')
     slug = models.SlugField('Название в виде url', max_length=200)
@@ -56,6 +53,7 @@ class Post(models.Model):
         'Tag',
         related_name='posts',
         verbose_name='Теги')
+    objects = PostQuerySet.as_manager()
 
     def __str__(self):
         return self.title
